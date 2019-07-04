e7ce18af6d63221c12edd28977165cd970b37331
Robin Schwob <robin.schwob@student.ecp.fr>
2019-07-04 11:34:03 +0200

    * new version of merg_doubles * adding keep_pos for csv with mainly positive rles * adding show_dcm_info * adding getNextFilePath

--- src/modules/files.py
+++ src/modules/files.py
@@ -1,7 +1,9 @@
+import random
 import pydicom
 from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
+from modules.mask_functions import merge_rles
 
 
 def restruct(src, dest):
@@ -25,10 +27,23 @@ def merge_doubles(old, new):
     df = pd.read_csv(old)
     new_df = pd.DataFrame(columns=['ImageId', 'EncodedPixels'])
     for k, id in enumerate(df['ImageId'].unique()):
-        rles = []
+        new_rle = ''
         for rle in df.loc[df['ImageId'] == id, 'EncodedPixels']:
-            rles.append(rle)
-        new_df.loc[k] = [id, ' '.join(rles)]
+            new_rle = merge_rles(new_rle, rle)
+        new_df.loc[k] = [id, new_rle]
+    new_df.to_csv(new, index=False)
+
+
+def keep_pos(old, new):
+    df = pd.read_csv(old)
+    new_df = pd.DataFrame(columns=['ImageId', 'EncodedPixels'])
+    for row in df.itertuples():
+        id = row.ImageId
+        rle = row.EncodedPixels
+        k = row.Index
+        if rle != '-1' or random.random() <= 0.1:
+            # keep all postive and only 10% of negative for segmentation
+            new_df.loc[k] = [id, rle]
     new_df.to_csv(new, index=False)
 
 
@@ -51,3 +66,46 @@ def show(img, figsize=(10, 10)):
     plt.axis('off')
     plt.imshow(img, cmap=plt.cm.bone)
     plt.show()
+
+
+def show_dcm_info(fn):
+    dataset = pydicom.dcmread(str(fn))
+    print("Filename.........:", fn)
+    print("Storage type.....:", dataset.SOPClassUID)
+    print()
+
+    pat_name = dataset.PatientName
+    display_name = pat_name.family_name + ", " + pat_name.given_name
+    print("Patient's name......:", display_name)
+    print("Patient id..........:", dataset.PatientID)
+    print("Patient's Age.......:", dataset.PatientAge)
+    print("Patient's Sex.......:", dataset.PatientSex)
+    print("Modality............:", dataset.Modality)
+    print("Body Part Examined..:", dataset.BodyPartExamined)
+    print("View Position.......:", dataset.ViewPosition)
+
+    if 'PixelData' in dataset:
+        rows = int(dataset.Rows)
+        cols = int(dataset.Columns)
+        print("Image size.......: {rows:d} x {cols:d}, {size:d} bytes".format(
+            rows=rows, cols=cols, size=len(dataset.PixelData)))
+        if 'PixelSpacing' in dataset:
+            print("Pixel spacing....:", dataset.PixelSpacing)
+
+
+def getNextFilePath(output_folder, base_name):
+    highest_num = 0
+    for f in output_folder.iterdir():
+        if f.is_file():
+            try:
+                f = str(f.with_suffix('').name)
+                if f.split('_')[:-1] == base_name.split('_'):
+                    split = f.split('_')
+                    file_num = int(split[-1])
+                    if file_num > highest_num:
+                        highest_num = file_num
+            except ValueError:
+                'The file name "%s" is incorrect. Skipping' % f
+
+    output_file = highest_num + 1
+    return output_file
